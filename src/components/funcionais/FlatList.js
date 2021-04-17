0861b2f41ee672e065b0f333793ed4a8a84d5fc7
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-17 18:01:55 -0300

    FlatList selecionável

--- src/components/funcionais/FlatList.js
+++ src/components/funcionais/FlatList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     SafeAreaView,
     View,
@@ -6,6 +6,7 @@ import {
     StyleSheet,
     Text,
     StatusBar,
+    TouchableOpacity,
 } from 'react-native';
 
 const DATA = [
@@ -23,20 +24,33 @@ const DATA = [
     },
 ];
 
-const Item = ({ title }) => (
-    <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-    </View>
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+        <Text style={[styles.title, textColor]}>{item.title}</Text>
+    </TouchableOpacity>
 );
 
 export default (props) => {
-    const renderItem = ({ item }) => <Item title={item.title} />;
+    const [selectedId, setSelectedId] = useState(null);
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+        const color = item.id === selectedId ? 'white' : 'black';
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.id)}
+                backgroundColor={{ backgroundColor }}
+                textColor={{ color }}
+            />
+        );
+    };
     return (
         <SafeAreaView style={styles.container}>
             <FlatList
                 data={DATA}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id}
+                extraData={selectedId}
             ></FlatList>
         </SafeAreaView>
     );
